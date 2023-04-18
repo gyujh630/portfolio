6a3f849c82516f4ecfcdda54e3a47f07d03fec63
gyujh <rbwjrbxo@ajou.ac.kr>
2023-04-18 22:09:19 +0900

    JS: work

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
 //스크롤 시 navbar 변경
 
 const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => { //스크롤 될때 {}안의 코드 실행
-    if(window.scrollY > navbarHeight) {
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    if (window.scrollY > 160) {
         navbar.classList.add('navbar--down');
-    }
-    else {
-        navbar.classList.remove('navbar--down')
+
+    } else {
+        navbar.classList.remove('navbar--down');
     }
 });
 
@@ -20,7 +20,6 @@ function scrollToSection(sectionName) {
     var section = document.getElementById(sectionName);
     section.scrollIntoView({behavior: "smooth"});
 }
-
 
 //스크롤에 따른 home fade 효과
 const home = document.querySelector('.home__container')
@@ -40,11 +39,45 @@ const home2 = document.querySelector('#home')
 const arrow = document.querySelector('.arrow')
 document.addEventListener('scroll', () => {
     const homeHeight = home2.getBoundingClientRect().height;
-    if(window.scrollY > homeHeight / 2) {
-        console.log(1)
+    if(window.scrollY > 160) {
         arrow.classList.add('visible');
     }
     else{
         arrow.classList.remove('visible');
     }
 });
+
+
+//work
+
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work_projects')
+const projects= document.querySelectorAll('.project')
+const remember = 1;
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter==null) return;
+
+    const active = document.querySelector('.category__btn.selected')
+    active.classList.remove('selected');
+
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+
+    target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }
+            else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 200);
+});
+
+
+
